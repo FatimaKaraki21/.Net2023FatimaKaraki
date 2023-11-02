9c230f8a85947a584ca49bd33f082bcc7d165995
FatimaKaraki <fkaraki21@gmail.com>
2023-11-02 11:38:10 +0200

    Shopping Cart

---
+++ ShoppingCart/ShoppingCart/Infrastructure/Components/CategoriesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Infrastructure.Components
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+
+        private readonly DataContext _context;
+
+        public CategoriesViewComponent(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _context.Categories.ToListAsync());
+
+        private Task<List<Category>> GetCategoriesAsync()
+        {
+            return _context.Categories.OrderBy(x => x.Sorting).ToListAsync();
+        }
+    }
+}
---
+++ ShoppingCart/ShoppingCart/Models/OrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ShoppingCart.Models.ViewModels;
+
+namespace ShoppingCart.Models
+{
+    public class OrderViewModel
+    {
+
+        public Order Order { get; set; }
+        public CartViewModel CartViewModel { get; set; }
+    }
+}
